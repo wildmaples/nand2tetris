a3b4454eba82ee2de6c9194f8f0fb90a632ba289
Maple Ong <maple.ong@shopify.com>
2021-05-12 22:03:21 -0400

    Add machine language for multiplication

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,26 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@2 // set index 2 to 0
+M=0
+
+(LOOP)
+@0
+D=M
+
+@END
+D;JEQ // if index 0 is < 0 then end loop
+
+@1
+D=M
+@2
+M=M+D // put sum of index 2 and index 1 back to index 2
+@0
+M=M-1
+
+@LOOP
+0;JMP // continue loop
+
+(END)
+@END
+0;JMP
