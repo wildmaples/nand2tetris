afc79074f950cab759ec3097916191390e41486f
Maple Ong <maple.ong@shopify.com>
2021-05-20 15:04:48 -0400

    Add machine language for Fill

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,56 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+(START)
+@SCREEN
+D=A
+@R0
+M=D
+
+(CHANGECOLOUR)
+@KBD // check keyboard
+D=M
+
+@R1
+M=0 // default to white
+
+@MAKEDARK // change to black
+D;JNE // if not 0
+
+(CONT)
+@R1
+A=M
+D=A // get the colour
+
+@R0 // pointer to screen memory address
+A=M
+M=D // change to the colour
+
+@R0
+M=M+1 // store new pointer
+
+@24576
+D=A
+@R0
+D=M-D
+
+@START // back to the start
+D;JEQ // if index is over @SCREEN + 8192 (8K)
+
+@CHANGECOLOUR
+0;JMP
+
+(ENDCHANGECOLOUR)
+@ENDCHANGECOLOUR
+0;JMP
+
+(MAKEDARK)
+@R1
+M=-1
+@CONT // go back to the loop
+0;JMP
